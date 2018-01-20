f48df9fc693e3728d419389b86e7246ff88df8c3
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2018-01-20 13:21:20 +0100

    init commit

---
+++ src/main/java/pl/sda/Main.java
@@ -0,0 +1,17 @@
+package pl.sda;
+
+public class Main {
+    public static void main(String[] args) {
+        String input = "+3+5+6--+5+6+9+4---+5+6+3----------";
+        MyStack myStack = new MyStack();
+        int i =0;
+        while(i < input.length()){
+            if(input.charAt(i++) == '+'){
+                char c = input.charAt(i++);
+                myStack.push(c);
+            } else if( input.charAt(i++) == '-') {
+                myStack.pop();
+            }
+        }
+    }
+}
---
+++ src/main/java/pl/sda/MyStack.java
@@ -0,0 +1,18 @@
+package pl.sda;
+
+public class MyStack {
+    private char[] stack = new char[10];
+    private int index = -1;
+
+    public void push(char c){
+
+    }
+
+    public char pop(){
+        return 'c';
+    }
+
+    public char peek(){
+        return 'c';
+    }
+}
